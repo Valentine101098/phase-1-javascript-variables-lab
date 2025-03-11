71f0d8c91dfc62d9269df854cb5cfbb7bcae8d47
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-03-11 12:17:45 +0300

    used cons and let to declare variables in js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+// companyName;
+function companyName() {
+const companyName = 'Scuber';
+}
+let mostProfitableNeighborhood = 'Chelsea'
+// mostProfitableNeighborhood = Chelsea;
+    // return 'Chelsea';
+// function mostProfitableNeighborhood() {
+//     let mostProfitableNeighborhood = 'Chelsea'
+// }
+let companyCeo = 'Susan Smith'
+
+
